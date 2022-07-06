3aa34fe2a1101c8576b137b8a8dc338ac6f51b7f
Hxrxn Suchakhri <hxrxn.suchakhri@gmail.com>
2022-07-07 02:06:39 +0700

    line

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 4000;
 app.get("/", (req, res) => {
-  res.json(`Serer is running on PORT : ${PORT}.`);
+  res.json(`Serer is running on PORT : ${port}.`);
 });
 app.post("/webhook", (req, res) => res.sendStatus(200));
 app.listen(port);
