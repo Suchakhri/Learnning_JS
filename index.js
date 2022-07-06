7e084a7737f7aeebfcf45f898d0dcedb48ef1cba
Hxrxn Suchakhri <hxrxn.suchakhri@gmail.com>
2022-07-07 02:00:43 +0700

    line

--- index.js
+++ index.js
@@ -1,53 +1,55 @@
+const axios = require("axios");
+const line = require("@line/bot-sdk");
 const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request");
 const app = express();
-const port = process.env.PORT || 4000;
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const PORT = process.env.PORT || 4000;
+const env = require("dotenv").config().parsed;
+app.use(express.json());
+
 const lineConfig = {
   channelAccessToken: env.CHANNEL_ACCESS_TOKEN,
   channelSecret: env.CHANNEL_SECRET,
 };
 
-// // create LINE SDK client
-// const client = new line.Client(lineConfig);
+// create LINE SDK client
+const client = new line.Client(lineConfig);
 
 app.get("/", (req, res) => {
   res.json(`Serer is running on PORT : ${PORT}.`);
 });
-app.post("/webhook", (req, res) => {
-  let reply_token = req.body.events[0].replyToken;
-  reply(reply_token);
-  res.sendStatus(200);
+
+app.post("/hook", line.middleware(lineConfig), (req, res) => {
+  try {
+    const events = req.body.events;
+    console.log("events ====>", events);
+    return events.length > 0
+      ? events.map((item) => handleEvent(item))
+      : res.status(200).send("OK");
+  } catch (err) {
+    res.status(500).end();
+  }
 });
-app.listen(port);
-function reply(reply_token) {
-  let headers = {
-    "Content-Type": "application/json",
-    Authorization: `Bearer {${lineConfig.channelAccessToken}}`,
-  };
-  let body = JSON.stringify({
-    replyToken: reply_token,
-    messages: [
-      {
-        type: "text",
-        text: "Hello",
-      },
-      {
-        type: "text",
-        text: "How are you?",
-      },
-    ],
-  });
-  request.post(
-    {
-      url: "https://api.line.me/v2/bot/message/reply",
-      headers: headers,
-      body: body,
-    },
-    (err, res, body) => {
-      console.log("status = " + res.statusCode);
-    }
-  );
-}
+
+app.post("/webhook", line.middleware(lineConfig), async (req, res) => {
+  try {
+    const events = req.body.events;
+    console.log("events ====>", events);
+    return events.length > 0
+      ? events.map((item) => handleEvent(item))
+      : res.status(200).send("OK");
+  } catch (err) {
+    res.status(500).end();
+  }
+});
+// event handler
+const handleEvent = async (event) => {
+  console.log(event);
+  return client.replyMessage(event.replyToken, { type: "text", text: "Test" });
+};
+
+app.listen(PORT, () => {
+  console.log(`Serer is running on PORT : ${PORT}.`);
+  console.log(`http://localhost:${PORT}`);
+});
+
+module.exports = app;
