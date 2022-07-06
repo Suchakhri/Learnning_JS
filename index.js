a77584a522d742d4ed664956318949dcb18eb14f
Hxrxn Suchakhri <hxrxn.suchakhri@gmail.com>
2022-07-07 02:11:51 +0700

    line

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const line = require("@line/bot-sdk");
 const env = require("dotenv").config().parsed;
 const app = express();
 const port = process.env.PORT || 4000;
+
+const lineConfig = {
+  channelAccessToken: env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: env.CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.Client(lineConfig);
 app.get("/", (req, res) => {
   res.json(`Serer is running on PORT : ${port}.`);
 });
