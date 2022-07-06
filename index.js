00dccbb1adb41d393173d3bc675e459f86275897
Hxrxn Suchakhri <hxrxn.suchakhri@gmail.com>
2022-07-07 01:58:43 +0700

    line

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
 const app = express();
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+const lineConfig = {
+  channelAccessToken: env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: env.CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.Client(lineConfig);
+
 app.get("/", (req, res) => {
   res.json(`Serer is running on PORT : ${PORT}.`);
 });
-app.post("/webhook", (req, res) => res.sendStatus(200));
-app.listen(PORT);
+app.post("/webhook", (req, res) => {
+  let reply_token = req.body.events[0].replyToken;
+  reply(reply_token);
+  res.sendStatus(200);
+});
+app.listen(port);
+function reply(reply_token) {
+  let headers = {
+    "Content-Type": "application/json",
+    Authorization: `Bearer {${lineConfig.channelAccessToken}}`,
+  };
+  let body = JSON.stringify({
+    replyToken: reply_token,
+    messages: [
+      {
+        type: "text",
+        text: "Hello",
+      },
+      {
+        type: "text",
+        text: "How are you?",
+      },
+    ],
+  });
+  request.post(
+    {
+      url: "https://api.line.me/v2/bot/message/reply",
+      headers: headers,
+      body: body,
+    },
+    (err, res, body) => {
+      console.log("status = " + res.statusCode);
+    }
+  );
+}
