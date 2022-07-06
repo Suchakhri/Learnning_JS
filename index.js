10d94d80909fc57021189964ce8eaa90ed6a4a06
Hxrxn Suchakhri <hxrxn.suchakhri@gmail.com>
2022-07-07 02:05:35 +0700

    line

--- index.js
+++ index.js
@@ -1,35 +1,8 @@
-const axios = require("axios");
-const line = require("@line/bot-sdk");
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 4000;
-const env = require("dotenv").config().parsed;
-app.use(express.json());
-
-const lineConfig = {
-  channelAccessToken: env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: env.CHANNEL_SECRET,
-};
-
-// create LINE SDK client
-const client = new line.Client(lineConfig);
-
+const port = process.env.PORT || 4000;
 app.get("/", (req, res) => {
   res.json(`Serer is running on PORT : ${PORT}.`);
 });
-
-app.post("/webhook", line.middleware(lineConfig), (req, res) => {
-  res.sendStatus(200);
-});
-// event handler
-const handleEvent = async (event) => {
-  console.log(event);
-  return client.replyMessage(event.replyToken, { type: "text", text: "Test" });
-};
-
-app.listen(PORT, () => {
-  console.log(`Serer is running on PORT : ${PORT}.`);
-  console.log(`http://localhost:${PORT}`);
-});
-
-module.exports = app;
+app.post("/webhook", (req, res) => res.sendStatus(200));
+app.listen(port);
