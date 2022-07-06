9b9abd0d81a047d7c75b4b9b23419d5335390af9
Hxrxn Suchakhri <hxrxn.suchakhri@gmail.com>
2022-07-07 01:42:25 +0700

    line

---
+++ index copy.js
@@ -0,0 +1,55 @@
+const axios = require("axios");
+const line = require("@line/bot-sdk");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4000;
+const env = require("dotenv").config().parsed;
+app.use(express.json());
+
+const lineConfig = {
+  channelAccessToken: env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: env.CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.Client(lineConfig);
+
+app.get("/", (req, res) => {
+  res.json(`Serer is running on PORT : ${PORT}.`);
+});
+
+app.post("/hook", line.middleware(lineConfig), (req, res) => {
+  try {
+    const events = req.body.events;
+    console.log("events ====>", events);
+    return events.length > 0
+      ? events.map((item) => handleEvent(item))
+      : res.status(200).send("OK");
+  } catch (err) {
+    res.status(500).end();
+  }
+});
+
+app.post("/webhook", line.middleware(lineConfig), async (req, res) => {
+  try {
+    const events = req.body.events;
+    console.log("events ====>", events);
+    return events.length > 0
+      ? events.map((item) => handleEvent(item))
+      : res.status(200).send("OK");
+  } catch (err) {
+    res.status(500).end();
+  }
+});
+// event handler
+const handleEvent = async (event) => {
+  console.log(event);
+  return client.replyMessage(event.replyToken, { type: "text", text: "Test" });
+};
+
+app.listen(PORT, () => {
+  console.log(`Serer is running on PORT : ${PORT}.`);
+  console.log(`http://localhost:${PORT}`);
+});
+
+module.exports = app;
