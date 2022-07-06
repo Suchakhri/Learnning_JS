25d7d502e22fd090755788947110479008134ba0
Hxrxn Suchakhri <hxrxn.suchakhri@gmail.com>
2022-07-07 02:09:58 +0700

    line

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const axios = require("axios");
+const line = require("@line/bot-sdk");
+const env = require("dotenv").config().parsed;
 const app = express();
 const port = process.env.PORT || 4000;
 app.get("/", (req, res) => {
   res.json(`Serer is running on PORT : ${port}.`);
 });
-app.post("/webhook", (req, res) => res.sendStatus(200));
+app.post("/webhook", (req, res) => {
+  res.sendStatus(200);
+});
 app.listen(port);
